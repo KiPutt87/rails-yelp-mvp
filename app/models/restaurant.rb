1bdfbb20d3dcb6d2bd6b28737f83e4a1f9cb56b8
@kiputt87.noreply.github.com <kjputt1@gmail.com>
2025-05-07 22:34:10 +0100

    AdvancedRoutersYelpApp

---
+++ app/models/restaurant.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# 'A restaurant must have a name, an address and a category.'
+# A restaurant’s category must belong to this fixed list: ["chinese", "italian", "japanese", "french", "belgian"].
+# When a restaurant is destroyed, all of its reviews must be destroyed as well.
+# A review must belong to a restaurant.
+# A review must have a content.
+# A review must have a rating.
+# A review’s rating must be a number between 0 and 5.
+# A review’s rating must be an integer. For example, a review with a rating of 2.5 should be invalid!
+
+class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, presence: true, inclusion: {in:["chinese", "italian", "japanese", "french", "belgian"]}
+end
